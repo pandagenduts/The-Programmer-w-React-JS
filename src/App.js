cd10d0cb98f1ab114f1391a1a00803b701393757
Donny Rendi <donny.rendi@yahoo.co.id>
2023-04-21 11:29:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Header from './components/header/header.js'
+import Advantages from './components/main/advantages';
+import Hero from './components/main/hero';
+import OurClients from './components/main/ourClients';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <OurClients />
+      <Advantages />
+    </>
+    
   );
 }
 
